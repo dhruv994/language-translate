16f4311ee4bd19d0710541eaa561f58cd4529b08
Dhruv <dhruv.sharma994@gmail.com>
2023-03-12 22:06:55 -0400

    added ...rest as a props in button

--- src/Button.js
+++ src/Button.js
@@ -6,9 +6,12 @@ export default function Button({ children,
     success,
     danger,
     outline,
-    rounded, warning }) {
+    rounded,
+    warning,
+    ...rest
+}) {
 
-    const classes = className('px-3 py-1.5 border', {
+    const classes = className(rest.className, 'flex items-center px-3 py-1.5 border', {
         'border-blue-600 bg-blue-500 text-white': primary,
         'border-gray-900 bg-gray-900 text-white': secondary,
         'border-green-500 bg-green-500 text-white': success,
@@ -20,18 +23,8 @@ export default function Button({ children,
         'text-gray-800': outline && secondary,
         'text-green-600': outline && success,
         'text-red-600': outline && danger,
-        'text-yellow-500': outline && warning,
-
-
-
-
+        'text-yellow-500': outline && warning
     });
-    return <button className={classes}>{children}</button>
+    return <button {...rest} className={classes}>{children}</button>
 }
 
-Button.propTypes = {
-    checkVariationValue: (props) => {
-        console.log(props)
-
-    }
-}
