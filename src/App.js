dfca6d4435840fdf0ce7005d5b523fdbfabef6ea
Dhruv <dhruv.sharma994@gmail.com>
2023-03-12 21:22:37 -0400

    added logic to show different color of butotns

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Button from './Button';
 export default  function App (){
 return ( <>
     <h1> Hello World</h1>
-    <div><Button success>Click Here </Button></div>
-    <div><Button secondary warning>Buy Now </Button></div>
+    <div><Button success rounded outline>Click Here </Button></div>
+    <div><Button warning>Buy Now </Button></div>
     <div><Button danger>See Deals </Button></div>
     <div><Button secondary>Turn Back </Button></div>
-    <div><Button primary >TUrn Ahead </Button></div>
+    <div><Button primary >Turn Ahead </Button></div>
+    <div><Button warning >Turn Ahead </Button></div>
     </>
 )
 }
--- src/Button.js
+++ src/Button.js
@@ -1,17 +1,40 @@
-import PropTypes from 'prop-types';
+import className from 'classnames';
 
-export default function Button ({children,
-primary,
-secondary,
-success,
-danger,
-outline,
-rounded}){
-    return <button>{children}</button>
+const finalClassName = className({
+
+})
+
+export default function Button({ children,
+    primary,
+    secondary,
+    success,
+    danger,
+    outline,
+    rounded, warning }) {
+
+    const classes = className('px-3 py-1.5 border', {
+        'border-blue-600 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-red-500 bg-red-500 text-white': danger,
+        'border-yellow-400 bg-yellow-500 text-white': warning,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-red-500': outline && danger,
+        'text-yellow-400': outline && warning,
+
+
+
+
+    });
+    return <button className={classes}>{children}</button>
 }
 
 Button.propTypes = {
-    checkVariationValue : (props) =>{
+    checkVariationValue: (props) => {
         console.log(props)
 
     }
