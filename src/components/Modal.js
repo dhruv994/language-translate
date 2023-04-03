0db5ef0a8f865fa9acf74d5118b1ac56de30b35e
Dhruv <dhruv.sharma994@gmail.com>
2023-04-02 22:26:05 -0400

    added styles in Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,12 @@
 export default function Modal() {
     return (
         <div>
-            Modal
+            <div className="absolute inset-0 bg-gray-300 opacity-80 ">
+
+            </div>
+            <div className="absolute inset-40 p-10 bg-white">
+                I'm a modal
+            </div>
         </div>
     )
 }
