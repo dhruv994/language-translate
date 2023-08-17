47a126f3f63d24621958366d85b2b380edf53a40
Dhruv <dhruv.sharma994@gmail.com>
2023-08-16 21:14:45 -0400

    added one more component

--- language-translate/media/src/App.js
+++ language-translate/media/src/App.js
@@ -1,3 +1,10 @@
-export default function App(){
-    return (<h1> Coming from APp.js</h1>)
+import UserList from "./components/UserList"
+
+export default function App() {
+    return (
+        <div className="container mx-auto">
+            <UserList />
+
+        </div>
+    )
 }
