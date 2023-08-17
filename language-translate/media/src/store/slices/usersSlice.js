18494186cdbd8e1178289d7842940ed457d0a758
Dhruv <dhruv.sharma994@gmail.com>
2023-08-17 12:06:58 -0400

    getting started with async thunk

--- language-translate/media/src/store/slices/usersSlice.js
+++ language-translate/media/src/store/slices/usersSlice.js
@@ -1,11 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const userSlice = createSlice({
-    name:'users',
-    initialState : [{
-        data :[]
+    name: 'users',
+    initialState: [{
+        data: [],
+        isLoading: false,
+        error: null
     }],
-    reducers:{}
+    reducers: {}
 })
 
 export const usersReducer = userSlice.reducer;
